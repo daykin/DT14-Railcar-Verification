c951d5f8bc443dec12c20fbe159a41d6ad5569ed
syaza <syaza@users.noreply.github.com>
2017-04-24 18:16:07 -0400

    Update railcar.h

--- SteelCoilVerification/railcar.h
+++ SteelCoilVerification/railcar.h
@@ -15,12 +15,12 @@ public:
     ~RailCar();
 
     // constructor for railcar
-    RailCar(const QString carID, QMap<QString, QString> coils);
+    RailCar(const QString carID, QMap<QString, QList<QString>> coils, QString verfy);
 
     //copy constructor
     RailCar(const RailCar &other);
 
-    void BuildCoilData(const QString carID, const QMap<QString, QString> coildata);
+    void BuildCoilData(const QString carID, const QMap<QString, QList<QString>> coildata);
 
     // get size of vector coils
     int VectCoilSize();
